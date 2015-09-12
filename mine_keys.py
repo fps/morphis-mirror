1a7708f827c08879bfdc9a2cf8abee7d55dc4233
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-09-12 16:50:17 +0200

    whitespace fix

--- mine_keys.py
+++ mine_keys.py
@@ -7,5 +7,5 @@ import mbase32
 while True:
     print("---")
     key = rsakey.RsaKey.generate(bits=4096)
-    print("priv:" + base58.encode(key._encode_key()))
+    print("priv: " + base58.encode(key._encode_key()))
     print("key: " + mbase32.encode(enc.generate_ID(key.asbytes())))
