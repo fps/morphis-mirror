0987312d792e2596eaf032171dcffbb74f00be96
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-09-13 09:45:44 +0200

    add kseistroup's changes

--- mine_keys.py
+++ mine_keys.py
@@ -1,11 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+import sys
+from hashlib import sha512
+ 
 import base58
 import rsakey
 import enc
-from hashlib import sha512
 import mbase32
-
+ 
+import re
+ 
+if len(sys.argv) > 1:
+    rx = re.compile(sys.argv[1])
+else:
+    rx = re.compile('.+')
+ 
 while True:
-    print("---")
     key = rsakey.RsaKey.generate(bits=4096)
-    print("priv: " + base58.encode(key._encode_key()))
-    print("key: " + mbase32.encode(enc.generate_ID(key.asbytes())))
+    k32 = mbase32.encode(enc.generate_ID(key.asbytes()))
+    if rx.match(k32):
+        print("---")
+        print("priv: " + base58.encode(key._encode_key()))
+        print("key:  " + k32)
