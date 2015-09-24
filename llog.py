41f8f8815e0c37a14d21c9c45d65a0d19263f559
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-09-24 11:02:09 +0200

    remove dependence on logging ini file

--- llog.py
+++ llog.py
@@ -10,18 +10,18 @@ logging_initialized = False;
 
 def init():
     global logging_initialized
-#    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s [%(module)s:%(lineno)d] %(message)s")
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s [%(module)s:%(lineno)d] %(message)s")
 
     if (logging_initialized):
         return
 
 #    print("Reading in logging.ini.")
-    config_file = "logging.ini"
-    if len(sys.argv) >= 3:
-        if sys.argv[1] == "-l":
-            config_file =  sys.argv[2]
-
-    logging.config.fileConfig(config_file)
+#    config_file = "logging.ini"
+#    if len(sys.argv) >= 3:
+#        if sys.argv[1] == "-l":
+#            config_file =  sys.argv[2]
+#
+#    logging.config.fileConfig(config_file)
     logger = logging.getLogger(__name__)
 #    logger.info("Logger initialized.")
     logging_initialized = True
