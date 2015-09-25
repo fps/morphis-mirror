cfc8bcdb9a4acb3c81392b3930225ad824ae09aa
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-09-25 03:17:50 +0200

    screw it :)

--- morphis/maalstroom/__init__.py
+++ morphis/maalstroom/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2014-2015  Sam Maloney.
 # License: GPL v2.
 
+__import__('pkg_resources').declare_namespace(__name__)
+
 import llog
 
 import asyncio
--- setup.py
+++ setup.py
@@ -3,51 +3,12 @@
 
 from setuptools import setup, find_packages
 
-import os
-import glob
-
-modules = [
-    "asymkey",
-    "base58",
-    "bittrie",
-    "brute",
-    "chordexception",
-    "chord_packet",
-    "chord",
-    "chord_tasks",
-    "client_engine",
-    "client",
-    "consts",
-    "db",
-    "dhgroup14",
-    "dmail",
-    "dsskey",
-    "enc",
-    "hashbench",
-    "kexdhgroup14sha1",
-    "kex",
-    "llog",
-    "mbase32",
-    "mcc",
-    "mn1",
-    "multipart",
-    "mutil",
-    "packet",
-    "peer",
-    "putil",
-    "rsakey",
-    "shell",
-    "node",
-    "sshexception",
-    "sshtype"
-]
-
 setup(
     name='morphis', 
-    version='0.8',
-    entry_points={'console_scripts': [ 'morphis_node=morphis.node:main' ] }, 
+    version='0.9',
+    entry_points = { 'console_scripts': [ 'morphis_node = morphis.node:main' ] }, 
     
-    packages = [ 'morphis', 'morphis/maalstroom' ],
+    packages = find_packages(),
 
     package_data = {
         'morphis': [ '*.ico' , 'VERSION', 'maalstroom/templates/main/*', 'maalstroom/templates/dmail/*' ]
